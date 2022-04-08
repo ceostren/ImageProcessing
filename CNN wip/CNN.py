beaecb12ed9e52c250d153cf2e62975aaaf777ee
ceo <ceo8099@umd.edu>
2022-04-08 18:48:29 -0400

    Added a message to CNN.py explaining the wip files future usage

--- CNN wip/CNN.py
+++ CNN wip/CNN.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+import tensorflow as tf
+from tensorflow.keras import datasets,layers,models
 
 def load_data(path, header):
     marks_df = pd.read_csv(path, header=header)
@@ -12,3 +14,4 @@ if __name__ == '__main__':
     X = data.iloc[:, :-1]
 
     y = data
+    print("This function will be updated to run an image thru an edge detection CNN")
